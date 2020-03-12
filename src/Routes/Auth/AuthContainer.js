4b8a91e6ec02af9a83688c8c09e9d5edeb970276
DS Hwang <hds2g22@gmail.com>
2020-03-12 11:20:09 +0900

    #5.5 Toastify

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -3,6 +3,7 @@ import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
 import { useMutation } from "react-apollo-hooks";
 import { LOG_IN } from "./AuthQueries";
+import { toast } from "react-toastify";
 
 export default () => {
   const [action, setAction] = useState("logIn");
@@ -10,6 +11,13 @@ export default () => {
   const lastName = useInput("");
   const email = useInput("");
   const [requestSecret] = useMutation(LOG_IN, {
+    update: (_, { data }) => {
+      const { requestSecret } = data;
+      if (!requestSecret) {
+        toast.error("You don't have an account, create one");
+        setTimeout(() => setAction("signUp), 3000"));
+      }
+    },
     variables: { email: email.value }
   });
 
